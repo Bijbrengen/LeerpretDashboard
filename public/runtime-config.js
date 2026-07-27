17ff779c958ee8700845f144a787870b3d9de679
Bijbrengen <slagter@bijbrengen.nl>
2026-07-27 16:46:18 +0200

    Slimme automatische detectie: localhost vs GitHub Pages tunnel URL

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,6 +1,14 @@
-window.LEERPRET_CONFIG = Object.freeze({
-  "apiBase": "http://127.0.0.1:47111/api",
-  "dashboardUrl": "http://127.0.0.1:47112/",
-  "editorUrl": "http://127.0.0.1:47114/",
-  "learngameOmUrl": "http://127.0.0.1:47113/"
-});
+(function() {
+  var isLocal = typeof window !== "undefined" && (
+    window.location.hostname === "127.0.0.1" ||
+    window.location.hostname === "localhost"
+  );
+  var tunnelUrl = "https://curly-pandas-dance.loca.lt/api"; // Vul hier eventueel je actieve tunnel-URL in
+  
+  window.LEERPRET_CONFIG = Object.freeze({
+    "apiBase": isLocal ? "http://127.0.0.1:47111/api" : tunnelUrl,
+    "dashboardUrl": isLocal ? "http://127.0.0.1:47112/" : "https://bijbrengen.github.io/LeerpretDashboard/",
+    "editorUrl": "http://127.0.0.1:47114/",
+    "learngameOmUrl": "http://127.0.0.1:47113/"
+  });
+})();
