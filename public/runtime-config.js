75d9919d8c90a576e8287a3a42ec6a871e9f6717
Bijbrengen <slagter@bijbrengen.nl>
2026-07-27 17:54:18 +0200

    Herstel actieve Cloudflare tunnel URL voor directe werking

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -3,10 +3,10 @@
     window.location.hostname === "127.0.0.1" ||
     window.location.hostname === "localhost"
   );
-  var prodApi = "https://api.leerpretpark.nl/api";
+  var tunnelUrl = "https://intent-carries-travelers-media.trycloudflare.com/api";
 
   window.LEERPRET_CONFIG = Object.freeze({
-    "apiBase": isLocal ? "http://127.0.0.1:47111/api" : prodApi,
+    "apiBase": isLocal ? "http://127.0.0.1:47111/api" : tunnelUrl,
     "dashboardUrl": isLocal ? "http://127.0.0.1:47112/" : "https://bijbrengen.github.io/LeerpretDashboard/",
     "editorUrl": isLocal ? "http://127.0.0.1:47114/" : "https://bijbrengen.github.io/LeerboxEditor/",
     "learngameOmUrl": isLocal ? "http://127.0.0.1:47113/" : "https://bijbrengen.github.io/Learngame-Operations-Management/"
